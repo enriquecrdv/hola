edf7ea16fc49fe8e7567fee01f0d288a266d113f
Irak Darek Ortiz Soqui <37721867+pirer48@users.noreply.github.com>
2021-10-20 13:29:42 -0700

    first commit

---
+++ HeWo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HeWo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
